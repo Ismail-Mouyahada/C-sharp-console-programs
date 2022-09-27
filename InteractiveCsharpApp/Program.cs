c2cdf814fc8e2cc71cf02d65742af33168e3c570
ISMAIL MOUYAHADA <66369128+Ismail-Mouyahada@users.noreply.github.com>
2022-09-27 20:46:12 +0200

    Recolte de données et affichage
    Récolter les données d'un utilisateur ensuite les afficher.

--- InteractiveCsharpApp/Program.cs
+++ InteractiveCsharpApp/Program.cs
@@ -5,8 +5,21 @@ namespace InteractiveCsharpApp // Note: actual namespace depends on the project 
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Bonjour tout le monde!");
+        {   
+
+            // Declaration des variables
+            string? age,nom,prenom;
+
+            // Reculte des données
+            Console.Write("veuillez renseigner votre age ? > ");
+            age = Console.ReadLine();
+            Console.Write("Votre nom respectif ? > ");
+            nom = Console.ReadLine();
+            Console.Write("Votre prénom respectif ? > ");
+            prenom = Console.ReadLine();
+
+            // logout data to the console
+            Console.WriteLine($"Bonjour, je m'appelle {nom} {prenom} et j'ai {age} ans !  ");
         } 
     }
 }
