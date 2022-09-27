1f656c9145893379e4634dfa8ca60091e890f6a3
ISMAIL MOUYAHADA <66369128+Ismail-Mouyahada@users.noreply.github.com>
2022-09-27 21:43:43 +0200

    Commentaires
    Ajouter des commentaires

--- InteractiveCsharpApp/Program.cs
+++ InteractiveCsharpApp/Program.cs
@@ -8,15 +8,22 @@ namespace InteractiveCsharpApp // Note: actual namespace depends on the project 
         {   
 
             // Declaration des variables
-            string? age,nom,prenom;
+            string?  age, nom,prenom;
+            
 
             // Reculte des données
+
             Console.Write("veuillez renseigner votre age ? > ");
             age = Console.ReadLine();
+
             Console.Write("Votre nom respectif ? > ");
             nom = Console.ReadLine();
+            
             Console.Write("Votre prénom respectif ? > ");
             prenom = Console.ReadLine();
+
+
+            
 
             // logout data to the console
             Console.WriteLine($"Bonjour, je m'appelle {nom} {prenom} et j'ai {age} ans !  ");
