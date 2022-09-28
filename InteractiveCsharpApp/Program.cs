cbf7144bee60faafe14909864bb5fb3a0efa06a0
ISMAIL MOUYAHADA <66369128+Ismail-Mouyahada@users.noreply.github.com>
2022-09-28 14:33:46 +0200

    Create ASP dotnet web-app
    create ASP dotnet web app

--- InteractiveCsharpApp/Program.cs
+++ InteractiveCsharpApp/Program.cs
@@ -1,32 +1,50 @@
 ﻿using System;
+using System.Net;
+using System.Web;
 
 namespace InteractiveCsharpApp // Note: actual namespace depends on the project name.
 {
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
 
-            // Declaration des variables
-            string?  age, nom,prenom;
-            
-
-            // Reculte des données
-
-            Console.Write("veuillez renseigner votre age ? > ");
-            age = Console.ReadLine();
-
-            Console.Write("Votre nom respectif ? > ");
-            nom = Console.ReadLine();
-            
-            Console.Write("Votre prénom respectif ? > ");
-            prenom = Console.ReadLine();
+            var client = new RestClient("https://v6.exchangerate-api.com/v6/e886397062b5c31bb26a553a/latest/EUR");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-api-key", " e886397062b5c31bb26a553a");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+        //     // Declaration des variables
+        //     string? nom, prenom;
+        //     int age;
 
 
-            
+        //     // Reculte des données
+        //     try
+        //     {
+        //         Console.Write("veuillez renseigner votre age ? > ");
+        //         age = int.Parse(Console.ReadLine());
+        //         Console.Write("Votre nom respectif ? > ");
+        //         nom = Console.ReadLine();
 
-            // logout data to the console
-            Console.WriteLine($"Bonjour, je m'appelle {nom} {prenom} et j'ai {age} ans !  ");
-        } 
-    }
+        //         Console.Write("Votre prénom respectif ? > ");
+        //         prenom = Console.ReadLine();
+
+
+        //     }
+        //     catch (System.Exception)
+        //     {
+
+        //         throw;
+        //     }
+
+
+
+
+        //     // logout data to the console
+        //     Console.WriteLine($"Bonjour, je m'appelle {nom} {prenom} et j'ai {age} ans, bientot vous aurez {age + 1} !  ");
+        // }
+    
 }
---
+++ Main-ASP-TD-Project-CESI/Main-ASP-TD-Project-CESI/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Main_ASP_TD_Project_CESI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
